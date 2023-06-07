7811f796f23d4c2c30e909cf18284edae5c4107f
bigboykirusha <105214100+bigboykirusha@users.noreply.github.com>
2023-06-07 22:18:19 +0300

    add About page, work with HOC

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#">
+        <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               className="w-3 h-3 rounded-full bg-secondary mb-1"
